e65dc34da4d5a792a2a640d36c62e1dae962ed93
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-03-19 18:21:27 +1100

    feat: nicer Folder exporer design

--- src/components/FolderExplorer.tsx
+++ src/components/FolderExplorer.tsx
@@ -1,4 +1,10 @@
 import Folder, { SubitemKind } from "@/api/types/Folder";
+import {
+  faAngleDown,
+  faAngleRight,
+  faFolder,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // This only renders subitems,
 // it is not responseible for rendering the original folder.
@@ -14,18 +20,32 @@ function FolderExporer({
   function drawFolder(folder: Folder) {
     return (
       <>
-        <ul>
+        <ul style={{ listStyleType: "none" }}>
           {folder.items.map((item) => {
             if (item.kind == SubitemKind.Folder) {
               // Basically guarentee this, hope there is a way
               // for this to make sense in typescript
               const subfolder = folders[item.itemID as number];
               const subitems = subfolder.open && drawFolder(subfolder);
-              const openIcon = subfolder.open ? "↡" : "↠";
+              const openIcon = subfolder.open ? (
+                <FontAwesomeIcon
+                  style={{ paddingRight: 10 }}
+                  color="gray"
+                  icon={faAngleDown}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  style={{ paddingLeft: 2, paddingRight: 12 }}
+                  color="gray"
+                  icon={faAngleRight}
+                />
+              );
               return (
                 <li key={subfolder.id}>
                   <a onClick={() => onDropdownClick(item.itemID as number)}>
-                    {openIcon} 📁 {subfolder.name}
+                    {openIcon}
+                    <FontAwesomeIcon icon={faFolder} color="gray" />{" "}
+                    {subfolder.name}
                   </a>
                   {subitems}
                 </li>
