d19b3e1e2b22ad66faf359c19a936fce8e7ea353
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-04-01 01:06:37 +1100

    fix: check if authenticated correctly

--- src/api/checkIsAuthenticated.ts
+++ src/api/checkIsAuthenticated.ts
@@ -1,10 +1,17 @@
 import { Auth } from "@api/firebase/createApp";
 
-const isFirebaseAuthenticated = () => Auth.authStateReady().then(() => Auth.currentUser != null);
-const isSpotifyAuthenticated = () => localStorage.getItem("token") != "";
+const isFirebaseAuthenticated = () =>
+  Auth.authStateReady().then(() => Auth.currentUser != null);
+const isSpotifyAuthenticated = () =>
+  localStorage.getItem("access_token") != "" &&
+  localStorage.getItem("access_token") != null;
 
 async function IsAuthenticated(): Promise<boolean> {
-    return (await isFirebaseAuthenticated() && isSpotifyAuthenticated());
+  return (await isFirebaseAuthenticated()) && isSpotifyAuthenticated();
 }
 
-export { IsAuthenticated as default, isFirebaseAuthenticated, isSpotifyAuthenticated };
+export {
+  IsAuthenticated as default,
+  isFirebaseAuthenticated,
+  isSpotifyAuthenticated,
+};
