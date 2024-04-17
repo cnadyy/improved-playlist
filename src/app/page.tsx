6baf867013dafb74482aee7a28d669f712eb3716
liam-brha <runningzoomcat@gmail.com>
2024-04-17 13:32:56 +1000

    feat: ground hog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
+import bg from "@/../public/niklas-tidbury-groundhog.jpg";
 
 export default function Home() {
     return (
-        <div>
-            <p>Improved spotify playlists</p>
-            <p>One day this might look pretty</p>
-            <Link href="/home">This goes over to the site!</Link>
-        </div>
+        <>
+            <main
+                style={{
+                    height: "100vh",
+                    width: "100%",
+                    backgroundImage: `url(${bg.src})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    overflow: "hidden",
+                }}
+            >
+                <p>Improved spotify playlists</p>
+                <p>One day this might look pretty</p>
+                <Link href="/home">This goes over to the site!</Link>
+            </main>
+            <footer>
+                <p>
+                    Photo by{" "}
+                    <a href="https://unsplash.com/@ntidbury?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                        Niklas Tidbury
+                    </a>{" "}
+                    on{" "}
+                    <a href="https://unsplash.com/photos/brown-beaver-V7ZLzhBbY0o?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                        Unsplash
+                    </a>
+                </p>
+            </footer>
+        </>
     );
 }
